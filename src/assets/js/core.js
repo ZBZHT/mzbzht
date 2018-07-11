24c8331d86dd46f27f27322896981561af6c6418
xiejiaxin <904073612@qq.com>
2018-07-11 20:41:42 +0800

    sign逻辑

--- src/assets/js/core.js
+++ src/assets/js/core.js
@@ -24,84 +24,84 @@ module.exports = {
      * w 表示星期几
      **/
   formatDate: function (text, date) {
-    var reg = /yyyy|yy|M+|d+|h+|m+|s+|q+|S|w/g;
+    var reg = /yyyy|yy|M+|d+|h+|m+|s+|q+|S|w/g
     text = text.replace(reg, function (pattern) {
-      var result;
+      var result
       switch (pattern) {
-        case "yyyy":
-          result = date.getFullYear();
-          break;
-        case "yy":
+        case 'yyyy':
+          result = date.getFullYear()
+          break
+        case 'yy':
           result = date.getFullYear().toString().substring(2);
-          break;
-        case "M":
-        case "MM":
-          result = date.getMonth() + 1;
-          break;
-        case "dd":
-        case "d":
-          result = date.getDate();
-          break;
-        case "hh":
-        case "h":
-          result = date.getHours();
-          break;
-        case "mm":
-        case "m":
-          result = date.getMinutes();
-          break;
-        case "ss":
-        case "s":
-          result = date.getSeconds();
-          break;
-        case "q":
-          result = Math.floor((date.getMonth() + 3) / 3);
-          break;
-        case "S":
-          result = date.getMilliseconds();
-          break;
-        case "w":
-          result = "日一二三四五六".charAt(date.getDay());
-          break;
+          break
+        case 'M':
+        case 'MM':
+          result = date.getMonth() + 1
+          break
+        case 'dd':
+        case 'd':
+          result = date.getDate()
+          break
+        case 'hh':
+        case 'h':
+          result = date.getHours()
+          break
+        case 'mm':
+        case 'm':
+          result = date.getMinutes()
+          break
+        case 'ss':
+        case 's':
+          result = date.getSeconds()
+          break
+        case 'q':
+          result = Math.floor((date.getMonth() + 3) / 3)
+          break
+        case 'S':
+          result = date.getMilliseconds()
+          break
+        case 'w':
+          result = '日一二三四五六'.charAt(date.getDay())
+          break
         default:
-          result = "";
-          break;
+          result = ''
+          break
       }
-      if (pattern.length == 2 && result.toString().length == 1) {
-        result = "0" + result;
+      if (pattern.length === 2 && result.toString().length === 1) {
+        result = '0' + result
       }
-      return result;
-  });
-  return text;
+      return result
+    })
+    return text
   },
   /**
    * 得到服务器信息
    */
-  getServerInfo: function() {
-      var ipv4 = [],mac = [];
-      var networkInterfaces = os.networkInterfaces();
-      for (var key in networkInterfaces) {
-          if (networkInterfaces.hasOwnProperty(key)) {
-              var interfaces = networkInterfaces[key];
-              for(var i = 0;i < interfaces.length; i++){
-                  if(interfaces[i].family == "IPv4" && !interfaces[i].internal){
-                      ipv4.push(interfaces[i].address);
-                      mac.push(interfaces[i].mac);
-                  }
-              }
+  getServerInfo: function () {
+    var ipv4 = [], mac = []
+    var networkInterfaces = os.networkInterfaces()
+    for (var key in networkInterfaces) {
+      if (networkInterfaces.hasOwnProperty(key)) {
+        var interfaces = networkInterfaces[key]
+        for (var i = 0; i < interfaces.length; i++) {
+          if (interfaces[i].family === 'IPv4' && !interfaces[i].internal){
+            ipv4.push(interfaces[i].address)
+            mac.push(interfaces[i].mac)
           }
+        }
       }
+    }
 
-      return{
-          host: os.hostname(),
-          ipv4: ipv4,
-          mac: mac
-      }
+    return {
+      host: os.hostname(),
+      ipv4: ipv4,
+      mac: mac
+    }
   },
   /**
    * 获取服务器时间
    */
-  getServerTime: function(){
+  getServerTime: function (){
       return this.formatDate("[yyyy.MM.dd hh:mm:ss]",new Date());
   },
 
--- src/pages/sign/Sign.vue
+++ src/pages/sign/Sign.vue
@@ -22,9 +22,12 @@
         </span>
       </el-dialog>
       <button class="btn"
-              @click="centerDialogVisible = true,stuSignIn()"
-              v-if="showBut === true">请点击签到</button>
-      <button class="nobtn" v-else>请在课前30分钟内签到</button>
+              v-if="showBut === true && showReadlyBut === true"
+              @click="centerDialogVisible = true,stuSignIn()">请点击签到</button>
+      <button class="nobtn" v-if="showBut === false && showReadlyBut === true">请在课前30分钟内签到</button>
+      <div v-if="showReadlyBut === false">
+        <button class="nobtn" @click="stuSignIn()">已签到</button>
+      </div>
     </section>
     <section class="sign-p">
       <p @click="backHome()" v-if="showBut === false">跳转至系统首页</p>
@@ -34,6 +37,7 @@
 <script>
 import axios from 'axios'
 import moment from 'moment'
+import core from '../../assets/js/core.js'
 export default {
   name: 'Sign',
   data () {
@@ -42,7 +46,8 @@ export default {
       course: '',
       stuCourse: '',
       centerDialogVisible: false,
-      showBut: true
+      showBut: true,
+      showReadlyBut: true
     }
   },
   mounted () {
@@ -51,19 +56,29 @@ export default {
         stuCourse: ''
       }
     }).then((res) => {
-      console.log(res)
       let resCourse = res.data.result
+      console.log(res.data)
       if (res.data.code === 0) {
-        let startTime = new Date(moment(resCourse.courseDate + ',' + resCourse.startTime).format("YYYY-MM-DD,HH:mm")).getTime();
-        let newTime = new Date().getTime() + 60 * 30 * 1000
+
+        let startTime = new Date(moment(resCourse.courseDate + ',' + resCourse.startTime).format('YYYY-MM-DD,HH:mm')).getTime() - 60 * 30 * 1000
+        let endTime = new Date(moment(resCourse.courseDate + ',' + resCourse.endTime).format('YYYY-MM-DD,HH:mm')).getTime()
+        let newTime = new Date().getTime()
         resCourse.date = resCourse.startTime + '~' + resCourse.endTime
         this.stuCourse = resCourse
-        if (newTime < startTime) {
-          this.showBut = false
-        } else {
+        if (startTime <= newTime && newTime < endTime) {
           this.showBut = true
+          console.log(this.showBut)
+        } else {
+          this.showBut = false
+          console.log(startTime)
+          console.log(newTime)
+          console.log(endTime)
         }
+        this.getFirstData()
         // console.log(this.showBut);
+
+      } else if (res.data.code === 1) {
+        this.showBut = false
       }
     })
   },
@@ -93,6 +108,24 @@ export default {
         // console.log(res.data.result);
         if (res.data.code === 0) {
           this.addSuccess('签到成功')
+          this.getFirstData()
+        }
+      })
+    },
+    getFirstData () {
+      axios.post('/teacherCMS/getSignInMsg', {
+        data: {
+          stuCourse: this.stuCourse
+        }
+      }).then((res) => {
+        // console.log(res.data.result);
+        if (res.data.code === 0) {
+          let resState = res.data.result.state;
+          if (resState === 0 || resState === 1 || resState === 2 || resState === 3) {
+            this.showReadlyBut = false
+          } else {
+            this.showReadlyBut = true
+          }
         }
       })
     },
